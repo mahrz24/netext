e1868ecf6e4536f72beffac57e16a789c97c7fb4
Malte Klemm <me@malteklemm.de>
2022-07-26 00:01:21 +0200

    Buffer rendering with z-index and overlaps works (needs some specific tests)

--- netext/terminal_graph.py
+++ netext/terminal_graph.py
@@ -35,7 +35,9 @@ class TerminalGraph(Generic[G]):
         nx.set_node_attributes(self._nx_graph, node_buffers, "_netext_node_buffer")
 
         # Position the nodes and add the position information to the graph
-        node_positions: Dict[Hashable, Tuple[float, float]] = layout_engine(self._nx_graph)
+        node_positions: Dict[Hashable, Tuple[float, float]] = layout_engine(
+            self._nx_graph
+        )
 
         # From the node buffer sizing and the layout, determine the total size needed for the graph.
         # TODO: We could add explicit sizing, also it is possible to recompute the edge buffer adaptively.
@@ -57,6 +59,7 @@ class TerminalGraph(Generic[G]):
             rasterize_edge(node_buffers[u], node_buffers[v], data)
             for u, v, data in self._nx_graph.edges(data=True)
         ]
+
 
     def _transform_node_positions_to_console(
         self, node_positions: Dict[Hashable, Tuple[float, float]]
@@ -120,7 +123,7 @@ class TerminalGraph(Generic[G]):
         buffers_by_row = defaultdict(list)
         for buffer in chain(node_buffers.values(), self.edge_buffers):
             buffers_by_row[buffer.top_y] = sorted(
-                buffers_by_row[buffer.top_y] + [((buffer.left_x, buffer))]
+                buffers_by_row[buffer.top_y] + [buffer]
             )
 
         active_buffers = []
@@ -130,46 +133,83 @@ class TerminalGraph(Generic[G]):
             active_buffers = sorted(
                 [
                     (
-                        bb_left_x
-                        - buffer.segments[buffer_row].x_offset
-                        + buffer.segments[buffer_row + 1].x_offset,
-                        z_index,
+                        buffer.left_x + buffer.segments[buffer_row + 1].x_offset,
+                        buffer.segments[buffer_row + 1].segment,
                         buffer_row + 1,
                         buffer,
                     )
-                    for bb_left_x, z_index, buffer_row, buffer in active_buffers
-                    if row < buffer.bottom_y
+                    for _, segment, buffer_row, buffer in active_buffers
+                    if row <= buffer.bottom_y
                 ]
             )
+
             new_active_buffers = sorted(
                 [
-                    (bb_left_x + buffer.segments[0].x_offset, buffer.z_index, 0, buffer)
-                    for bb_left_x, buffer in buffers_by_row[row]
+                    (
+                        buffer.left_x + buffer.segments[0].x_offset,
+                        buffer.segments[0].segment,
+                        0,
+                        buffer,
+                    )
+                    for buffer in buffers_by_row[row]
                 ]
             )
-            active_buffers = list(
-                merge(active_buffers, new_active_buffers)
-            )
+
+            active_buffers = list(merge(active_buffers, new_active_buffers))
+
             if not active_buffers:
                 yield Segment(" " * self.width + "\n")
                 continue
+
             current_x = 0
 
-            # TODO: There is still some problem here as can be seen in the payley graph
-            # Some overlaps seem to push lines to far
-            for bb_left_x, z_index, buffer_row, buffer in active_buffers:
-                # We need to cut the segment and only print the non overlapped part if the current x coordinate 
+            working_buffers = list(active_buffers)
+            # print(working_buffers)
+            while working_buffers:
+                segment_left_x, segment, buffer_row, buffer = working_buffers.pop(0)
+
+                full_segment_cell_length = segment.cell_length
+
+                # Empty segments should be ignored, though ideally we should not store them
+                # in the buffer at all.
+                if full_segment_cell_length == 0:
+                    continue
+
+                # We need to cut the segment and only print the non overlapped part if the current x coordinate
                 # is already the buffer's left boundary.
-                segment: Segment = buffer.segments[buffer_row].segment
-                segment_cell_length = buffer.segments[buffer_row].segment.cell_length
-                if current_x > bb_left_x:
-                    # TODO: We already sort by z-index, but if we have a z-index overlap
-                    # We need to maintain a look-ahead z-index buffer to see if we have to cut more
-                    segment = segment.split_cells(current_x-bb_left_x)[1]
+                if current_x >= segment_left_x:
+                    segment = segment.split_cells(current_x - segment_left_x)[1]
                 else:
-                    yield Segment(" " * (bb_left_x - current_x))
-                yield segment
-                current_x = bb_left_x + segment_cell_length
+                    # Pad to the left boundary of the segment
+                    yield Segment(" " * (segment_left_x - current_x))
+                    current_x = segment_left_x
+
+                # Perform a look ahead, and if the left boundary of any of the next active buffers
+                # intersects with the current buffer & length (and has a smaller z-index), we split the
+                # current buffer segment at that place and add the remaining part after the intersecting
+                # segment.
+                for i, (segment_left_x_next, _, _, buffer_next) in enumerate(
+                    working_buffers
+                ):
+                    if (
+                        segment_left_x_next <= segment_left_x + full_segment_cell_length
+                        and buffer_next.z_index < buffer.z_index
+                    ):
+                        segment, overflow_segment = segment.split_cells(
+                            segment_left_x_next - segment_left_x
+                        )
+                        working_buffers.insert(
+                            i + 1,
+                            (segment_left_x_next, overflow_segment, buffer_row, buffer),
+                        )
+                        break
+
+                # If the overlap from a prior segment or the overlap of an upcoming
+                # segment (with lower z-index) cut the segment to disappear, nothing
+                # will be yielded.
+                if segment.cell_length > 0:
+                    yield segment
+                    current_x += segment.cell_length
 
             if current_x < self.width:
                 yield Segment(" " * (self.width - current_x))
