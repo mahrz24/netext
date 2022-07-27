f573da1f04698606343b67d8db866ea0eaa65e79
Malte Klemm <me@malteklemm.de>
2022-07-27 20:23:59 +0200

    Add pyright and fix some typing issues

--- netext/layout_engines/engine.py
+++ netext/layout_engines/engine.py
@@ -1,2 +1,9 @@
-class LayoutEngine:
-    pass
+from typing import Dict, Generic, Hashable, Tuple, TypeVar
+import networkx as nx
+
+G = TypeVar("G", nx.Graph, nx.DiGraph)
+
+
+class LayoutEngine(Generic[G]):
+    def __call__(self, graph: G) -> Dict[Hashable, Tuple[float, float]]:
+        return NotImplemented
