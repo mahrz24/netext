a12faef624c29b9a26cf0c90b3b2dd6afd1ce992
Malte Klemm <me@malteklemm.de>
2022-07-29 10:22:02 +0200

    First rendering benchmark added

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,6 +1,11 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+from networkx import binomial_tree
+from rich.console import Console
+
+from netext import TerminalGraph
+
 
 class TimeSuite:
     """
@@ -9,24 +14,10 @@ class TimeSuite:
     """
 
     def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+        self.graph = binomial_tree(4)
+        self.terminal_graph = TerminalGraph(self.graph)
+        self.console = Console()
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
-
-    def time_values(self):
-        for value in self.d.values():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            _ = d[key]
-
-
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+    def time_render_binomial_tree_4(self):
+        with self.console.capture():
+            self.console.print(self.terminal_graph)
