be8d61555f20cbd74191158d8f77060389e32769
Malte Klemm <me@malteklemm.de>
2022-08-16 14:23:03 +0200

    Refactor bresenham, add todos

--- TODO.md
+++ TODO.md
@@ -12,7 +12,7 @@
 # Refactoring
 
 * ~~Extract buffer renderer to own module to make it better testable~~
-* Clean up bresenham line drawing algorithm
+* ~~Clean up bresenham line drawing algorithm~~
 
 # Tests & Benchmarks
 
@@ -26,11 +26,19 @@
 # Features
 
 * Different edge routing algorithms
+    * Direct connection
     * Straight lines
 * Better line rendering
-    * Use unicode block characters and or slashes & dashes for lines
+    * Use magnets on the node border for edge positions
+    * Edge routing gives multiple segments of an edge, each segment can be drawn using different algorithms
+        * Box characters (straight lines only) with corner connection (only for straight lines)
+            * For this mode straight lines or direct connection should not matter
+        * General, lines draw into a bitmap buffer, that is turned into segments (makes braille box drawing easier to use)
+            * Simple mode (dots along the lines)
+            * Braille (not for straight lines)
 * ~~Attributes for nodes~~
 * Attributes for edges
 * ~~Custom node rendering~~
 * Edge labels
+* Node labels (not part of the placement algorithm)
 * Directed edges (arrow heads)
--- netext/edge_rasterizer.py
+++ netext/edge_rasterizer.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, List
 
 from rich.console import Console
 from rich.segment import Segment
@@ -57,91 +57,110 @@ def rasterize_edge(
     x1 = v_buffer.x
     y1 = v_buffer.y
 
-    offset_segments = []
+    segment_lines = _bresenham_line_drawing(x0, y0, x1, y1)
+
+    edge_buffer = EdgeBuffer(
+        z_index=0,
+        start=Point(x=x0, y=y0),
+        end=Point(x=x1, y=y1),
+        segment_lines=segment_lines,
+    )
+
+    return edge_buffer
+
+
+def _bresenham_line_drawing(x0: int, y0: int, x1: int, y1: int) -> List[OffsetLine]:
+    segment_lines = []
 
     if abs(y1 - y0) < abs(x1 - x0):
         if x0 > x1:
             x1, x0 = x0, x1
             y1, y0 = y0, y1
 
-        dx = x1 - x0
-        dy = y1 - y0
+        segment_lines = _bresenham_flat(x0, y0, x1, y1)
+    else:
+        if y0 > y1:
+            x1, x0 = x0, x1
+            y1, y0 = y0, y1
 
-        reverse = False
+        segment_lines = _bresenham_steep(x0, y0, x1, y1)
 
-        if dy < 0:
-            reverse = True
-            dy = -dy
+    for i, offset_segment in enumerate(segment_lines):
+        offset_segment.y_offset = i
 
-        D = 2 * dy - dx
-        y = y0
+    return segment_lines
 
-        last_offset = 0
-        current_segment = ""
 
-        for x in range(x0, x1 + 1):
-            current_segment += "-"
-            if D > 0:
-                offset_segments.append(
-                    OffsetLine(
-                        x_offset=last_offset,
-                        y_offset=0,
-                        segments=[Segment(current_segment, Style(color="green"))],
-                    )
-                )
-                current_segment = ""
-                last_offset = x - x0
-                D = D - 2 * dx
-            D = D + 2 * dy
+def _bresenham_flat(x0: int, y0: int, x1: int, y1: int) -> List[OffsetLine]:
+    segment_lines = []
+    dx = x1 - x0
+    dy = y1 - y0
 
-        if current_segment:
-            offset_segments.append(
+    reverse = False
+
+    if dy < 0:
+        reverse = True
+        dy = -dy
+
+    D = 2 * dy - dx
+
+    last_offset = 0
+    current_segment = ""
+
+    for x in range(x0, x1 + 1):
+        current_segment += "*"
+        if D > 0:
+            segment_lines.append(
                 OffsetLine(
                     x_offset=last_offset,
                     y_offset=0,
                     segments=[Segment(current_segment, Style(color="green"))],
                 )
             )
+            current_segment = ""
+            last_offset = x - x0
+            D = D - 2 * dx
+        D = D + 2 * dy
 
-        if reverse:
-            offset_segments = list(reversed(offset_segments))
-    else:
-        if y0 > y1:
-            x1, x0 = x0, x1
-            y1, y0 = y0, y1
-
-        dx = x1 - x0
-        dy = y1 - y0
-        xi = 1
-        x = 0
-        if dx < 0:
-            x = x0 - x1
-            xi = -1
-            dx = -dx
-        D = (2 * dx) - dy
-
-        for y in range(y0, y1 + 1):
-            offset_segments.append(
-                OffsetLine(
-                    x_offset=x,
-                    y_offset=0,
-                    segments=[Segment("|", Style(color="green"))],
-                )
+    if current_segment:
+        segment_lines.append(
+            OffsetLine(
+                x_offset=last_offset,
+                y_offset=0,
+                segments=[Segment(current_segment, Style(color="green"))],
             )
-            if D > 0:
-                x = x + xi
-                D = D + (2 * (dx - dy))
-            else:
-                D = D + 2 * dx
+        )
 
-    for i, offset_segment in enumerate(offset_segments):
-        offset_segment.y_offset = i
+    if reverse:
+        segment_lines = list(reversed(segment_lines))
 
-    edge_buffer = EdgeBuffer(
-        z_index=0,
-        start=Point(x=x0, y=y0),
-        end=Point(x=x1, y=y1),
-        segment_lines=offset_segments,
-    )
+    return segment_lines
 
-    return edge_buffer
+
+def _bresenham_steep(x0: int, y0: int, x1: int, y1: int) -> List[OffsetLine]:
+    segment_lines = []
+    dx = x1 - x0
+    dy = y1 - y0
+    xi = 1
+    x = 0
+    if dx < 0:
+        x = x0 - x1
+        xi = -1
+        dx = -dx
+    D = (2 * dx) - dy
+
+    for y in range(y0, y1 + 1):
+        segment_lines.append(
+            OffsetLine(
+                x_offset=x,
+                y_offset=0,
+                segments=[Segment("*", Style(color="green"))],
+            )
+        )
+        if D > 0:
+            x = x + xi
+            D = D + (2 * (dx - dy))
+        else:
+            D = D + 2 * dx
+
+    return segment_lines
