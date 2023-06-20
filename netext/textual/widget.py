4013d53bd983f26bbda16ad16ebdd110f0209906
Malte Klemm <me@malteklemm.de>
2023-06-20 17:18:45 +0200

    Widget works with resizing, but not a scrollview widget yet

--- netext/textual/widget.py
+++ netext/textual/widget.py
@@ -8,7 +8,7 @@ from textual.strip import Strip
 from netext import ConsoleGraph
 from netext.buffer_renderer import render_buffers
 from netext.console_graph import G
-
+from rich.segment import Segment
 
 class Graph(ScrollView, Generic[G]):
     graph: reactive[G | None] = reactive(None)
@@ -23,23 +23,31 @@ class Graph(ScrollView, Generic[G]):
         **console_graph_kwargs
     ):
         self._console_graph_kwargs = console_graph_kwargs
+        self._console_graph = None
         super().__init__(name=name, id=id, classes=classes, disabled=disabled)
         self.graph = graph
         self._strips = None
-        self._timer = None
+        self._timer = self.set_timer(0, self._resized)
 
     def on_resize(self, message: Resize):
         self.log("Graph was resized.")
-        # For now we simply update the graph as well
         if self._timer is not None:
             self._timer.stop()
-        self._timer = self.set_timer(1, self._graph_was_updated)
-        self._graph_was_updated()
+        self._timer = self.set_timer(1, self._resized)
 
+    def _resized(self):
+        if self.graph is not None and self._console_graph is None:
+            self._setup_console_graph()
+
+        if self._console_graph is not None and self.size.width != 0 and self.size.height != 0:
+            self._console_graph.max_width = self.size.width
+            self._console_graph.max_height = self.size.height
+            self._strip_segments = self.pre_render_strips()
+            self.refresh()
     def watch_graph(self, old_graph: G | None, new_graph: G | None) -> None:
         self._graph_was_updated()
 
-    def _graph_was_updated(self):
+    def _setup_console_graph(self):
         if self.graph is not None and self.size.width != 0 and self.size.height != 0:
             self._console_graph = ConsoleGraph(
                 self.graph,
@@ -49,14 +57,25 @@ class Graph(ScrollView, Generic[G]):
                 **self._console_graph_kwargs
             )
 
-            all_buffers = list(self._console_graph._all_current_lod_buffers())
-            self._strip_segments = render_buffers(
-                all_buffers, self._console_graph._viewport_with_constraints()
-            )
+    def _graph_was_updated(self):
+        self._setup_console_graph()
+
+        if self._console_graph is not None:
+            self._strip_segments = self.pre_render_strips()
             self.refresh()
         else:
             self._console_graph = None
             self._strip_segments = []
+
+
+    def pre_render_strips(self) -> list[list[Segment]]:
+        if self._console_graph is not None:
+            all_buffers = list(self._console_graph._all_current_lod_buffers())
+            return render_buffers(
+                    all_buffers, self._console_graph._viewport_with_constraints()
+                )
+        else:
+            return []
 
     def render_line(self, y: int) -> Strip:
         scroll_x, scroll_y = self.scroll_offset  # The current scroll position
--- playground/textual_playground.py
+++ playground/textual_playground.py
@@ -1,21 +1,32 @@
 from netext.textual.widget import Graph
 from netext import AutoZoom
 from textual.app import App, ComposeResult
-
+from typing import cast
+from rich.style import Style
+from rich import box
+from netext.edge_routing.modes import EdgeRoutingMode
+from netext.edge_rendering.modes import EdgeSegmentDrawingMode
+from netext.edge_rendering.arrow_tips import ArrowTip
+from textual.containers import Horizontal, Container
+from textual.widgets import Static
 import networkx as nx
 
-g = nx.Graph()
-g.add_node("Hello")
-g.add_node("World")
-g.add_edge("Hello", "World")
+g = cast(nx.Graph, nx.binomial_tree(6))
+
+nx.set_node_attributes(g, Style(color="blue"), "$content-style")
+nx.set_node_attributes(g, box.SQUARE, "$box-type")
+nx.set_edge_attributes(g, Style(color="red"), "$style")
+nx.set_edge_attributes(g, EdgeRoutingMode.ORTHOGONAL, "$edge-routing-mode")
+nx.set_edge_attributes(g, EdgeSegmentDrawingMode.BOX, "$edge-segment-drawing-mode")
+nx.set_edge_attributes(g, ArrowTip.ARROW, "$end-arrow-tip")
+nx.set_edge_attributes(g, ArrowTip.ARROW, "$start-arrow-tip")
 
 
 class GraphApp(App):
     CSS_PATH = "textual_playground.css"
 
     def compose(self) -> ComposeResult:
-        yield Graph(g, zoom=AutoZoom.FIT)
-
+        yield Horizontal(Graph(g, zoom=AutoZoom.FIT))
 
 app = GraphApp()
 
