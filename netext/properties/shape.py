2e4876eafb78172292179174eb44420d93758a5d
Malte Klemm <me@malteklemm.de>
2024-02-02 12:42:17 +0100

    Remove todo

--- netext/properties/shape.py
+++ netext/properties/shape.py
@@ -2,9 +2,6 @@ from dataclasses import dataclass
 from rich.box import Box as RichBox, ROUNDED
 
 
-# TODO I am not happy with the API here, to be improved
-# Call these classes shapes directly and remove the internal shape classes
-# As they are not needed only a shape buffer
 @dataclass
 class ShapeProperties:
     pass
