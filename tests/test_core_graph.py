67e191a3bbd947fdd13d1e6855cfdf9cc10930d2
Malte Klemm <me@malteklemm.de>
2024-04-24 13:51:24 +0200

    Fix the node data or default method

--- tests/test_core_graph.py
+++ tests/test_core_graph.py
@@ -1,6 +1,26 @@
+import pytest
 from netext._core import CoreGraph
 
 
-def test_initialization():
-    g = CoreGraph.from_edges([(1, 2), (2, 3), (3, 4)])
-    assert set(g.all_nodes()) == {1, 2, 3, 4}
+@pytest.fixture
+def simple_graph():
+    return CoreGraph.from_edges([(1, 2), (2, 3), (3, 4)])
+
+
+def test_initialization(simple_graph: CoreGraph):
+    assert set(simple_graph.all_nodes()) == {1, 2, 3, 4}
+    assert set(simple_graph.all_edges()) == {(1, 2), (2, 3), (3, 4)}
+
+
+def test_adding_data(simple_graph: CoreGraph):
+    data = {"foo": "bar"}
+    simple_graph.update_node_data(1, data)
+    assert simple_graph.node_data(1) == data
+
+
+def test_adding_and_mutating_original_data(simple_graph: CoreGraph):
+    data = {"foo": "bar"}
+    simple_graph.update_node_data(1, data)
+    data["bar"] = "baz"
+    assert simple_graph.node_data(1) == data
+    assert simple_graph.node_data_or_default(1, None) == data
