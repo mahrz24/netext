38c47ff88674474de7c4abb1cee427cd0a868a53
Malte Klemm <me@malteklemm.de>
2024-05-03 14:37:04 +0200

    Continued a bit the static layout

--- src/layout/static_.rs
+++ src/layout/static_.rs
@@ -1,12 +1,11 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyDict};
 
 use crate::{geometry::Point, graph::CoreGraph};
 
 use super::LayoutEngine;
 
 #[pyclass(extends=LayoutEngine, subclass)]
-pub struct StaticLayout {
-}
+pub struct StaticLayout {}
 
 #[pymethods]
 impl StaticLayout {
@@ -15,15 +14,33 @@ impl StaticLayout {
         (StaticLayout {}, LayoutEngine {})
     }
 
-    fn layout(&self, graph: &CoreGraph) -> PyResult<Vec<(PyObject, Point)>> {
-        let mut node_data = Vec::new();
+    fn layout(&self, py: Python<'_>, graph: &CoreGraph) -> PyResult<Vec<(&PyObject, Point)>> {
+        let mut node_positions = Vec::new();
         for node in graph.all_nodes() {
-            let node_data = graph.node_data(node)?;
-            let x = node_data.get("$x").unwrap_or(&0.0);
-            let y = node_data.get("$y").unwrap_or(&0.0);
-            let point = Point::new(*x, *y);
-            node_data.push((node, point));
+            let node_ref = node.clone();
+            let node_data = graph
+                .node_data(&node_ref.into_bound(py))?
+                .unwrap()
+                .into_bound(py);
+            if node_data.is_instance_of::<PyDict>() {
+                let dict = node_data.downcast::<PyDict>()?;
+                let x = dict.get_item("$x")?.unwrap();
+                let y = dict.get_item("$y")?.unwrap();
+
+                let x_val = match x.extract::<i32>() {
+                    Ok(value) => value,
+                    Err(_) => 0,
+                };
+
+                let y_val = match y.extract::<i32>() {
+                    Ok(value) => value,
+                    Err(_) => 0,
+                };
+
+                let point = Point::new(x_val, y_val);
+                node_positions.push((node, point));
+            }
         }
-        Ok(node_data)
+        Ok(node_positions)
     }
 }
