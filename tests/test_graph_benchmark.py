982c067376f0afd8e85d12cedb84a5d08c90d4a3
Malte Klemm <me@malteklemm.de>
2025-02-07 21:14:07 +0100

    Added codspeed

---
+++ tests/test_graph_benchmark.py
@@ -0,0 +1,30 @@
+from networkx import binomial_tree
+import networkx as nx
+import pytest
+from statistics import median
+from rich.console import Console
+
+from netext import ConsoleGraph
+from netext.edge_rendering.modes import EdgeSegmentDrawingMode
+from netext.edge_routing.modes import EdgeRoutingMode
+
+@pytest.fixture(scope='module')
+def graph() -> ConsoleGraph:
+    graph = binomial_tree(5)
+
+    nx.set_edge_attributes(graph, EdgeRoutingMode.ORTHOGONAL, "$edge-routing-mode")
+    nx.set_edge_attributes(
+        graph,
+        EdgeSegmentDrawingMode.BOX,
+        "$edge-segment-drawing-mode",
+    )
+
+    return ConsoleGraph(graph)
+
+@pytest.fixture(scope='module')
+def console() -> Console:
+    return Console()
+
+@pytest.mark.benchmark
+def test_graph_performance(graph: ConsoleGraph, console: Console, benchmark):
+    benchmark(console.print, graph)
