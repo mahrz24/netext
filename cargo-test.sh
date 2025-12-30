f2114e5a1e3333017e96b2a1eaf0b2dc4a3008dd
Malte Klemm <me@malteklemm.de>
2025-12-30 09:00:32 +0100

    Fixing cargo tests on macos

--- cargo-test.sh
+++ cargo-test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 python_bin="${PYTHON_BIN:-python}"
 
 python_libdir="$("$python_bin" -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR") or "")')"
-python_version="$("$python_bin" -c 'import sys; print(f\"{sys.version_info.major}.{sys.version_info.minor}\")')"
+python_version="$("$python_bin" -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
 
 export PYO3_CROSS_PYTHON_VERSION="${PYO3_CROSS_PYTHON_VERSION:-$python_version}"
 
